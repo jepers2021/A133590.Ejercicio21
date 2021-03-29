9ae8c4a94ed5b999c849820dad708a5fca586e1e
Jorge <jepers2021@gmail.com>
2021-03-29 14:37:52 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace A133590.Ejercicio21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio 21");
+            Console.WriteLine("Programa que presenta al usuario la fecha actual.");
+
+            Console.WriteLine($"La fecha actual es: {DateTime.Now}");
+
+            Console.WriteLine("Presione cualquier tecla para salir..");
+            Console.ReadKey();
+        }
+    }
+}
